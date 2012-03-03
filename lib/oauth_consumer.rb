21ecf80d2c20070c9fb003551bebb533e6b3180d
asktkhr <bbclapton@gmail.com>
2012-03-03 22:43:56 +0900

    add description

--- lib/oauth_consumer.rb
+++ lib/oauth_consumer.rb
@@ -1,7 +1,7 @@
 require 'oauth'
 module OauthConsumer
-  CONSUMER_KEY = "*****************"
-  CONSUMER_SECRET = "********************"
+  CONSUMER_KEY = "*****************" #youRoomから取得したCONSUMER_KEYを設定
+  CONSUMER_SECRET = "********************" #youRoomから取得したCONSUMER_SECRETを設定
 
   def oauth_consumer
     OAuth::Consumer.new(CONSUMER_KEY,CONSUMER_SECRET, :site => "http://youroom.in")
